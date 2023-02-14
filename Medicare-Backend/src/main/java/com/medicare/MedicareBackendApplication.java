441d6ba5b080157f0d69c737cdeb8731cc7eef92
twarit soni <twarit.s88@gmail.com>
2023-02-14 12:21:57 +0530

    created spring boot project

---
+++ Medicare-Backend/src/main/java/com/medicare/MedicareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareBackendApplication.class, args);
+	}
+
+}
---
+++ Medicare-Backend/src/test/java/com/medicare/MedicareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
